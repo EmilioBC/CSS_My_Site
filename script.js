3cb963d9043d8cf96377502456b3b84240fc1e99
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-11-24 02:28:36 +0000

    init

---
+++ script.js
@@ -0,0 +1,45 @@
+// function loadUsers() {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET','index.html',true);
+//   xhr.onload = function() {
+//     if(this.status == 200){
+//       var users = JSON.parse(this.responseText);
+//       var output = '';
+//       for(var i in users){
+//         var output +=
+//         '<div class="loadUsers>'+
+//         '<img src="'+avatar_url+'" width="78" height="78"></img>'+
+//         '<ul>'+
+//         '<li>ID: '+users.id+'</li>'+
+//         '<li>Login: '+users.login+'</li>'+
+//         '</ul>'+
+//         '</div>';
+//       }
+//       document.getElemetnById('users').innerHTML = output;
+//     }
+//   }
+//   xhr.send();
+// }
+
+
+function loadUsers() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET','index.html',true);
+  xhr.onload = function() {
+    if(this.status == 200) {
+      var users = JSON.parse(this.responseText);
+      var output = '';
+      for (var i in users) {
+        var output +=
+        '<div class="users">'+
+        '<img src="'+avatar_url+'">'+
+        '<ul>'+
+        '<li>ID: '+users.id+'</li>'+
+        '</ul>'+
+        '</div>';
+      } 
+      document.getElementById('users').innerHTML = output;
+    }
+  }
+  xhr.send();
+}
